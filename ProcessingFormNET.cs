dfa5c3431d091c8cc254ec789687b8eb65795e4a
sweetnight <adhara.putra@gmail.com>
2024-09-22 02:15:15 +0700

    Initial commit for Magic.ProcessingFormNET

---
+++ ProcessingFormNET.cs
@@ -0,0 +1,233 @@
+﻿using System.Windows.Forms;
+using Magic.FormsNET;
+
+namespace Magic
+{
+    public partial class ProcessingFormNET : Form
+    {
+        private readonly DropShadow _dropShadow = new DropShadow();
+
+        private int ParamType { get; set; } = 0;
+        public Action? Worker { get; set; }
+        public Action<string>? Worker1String { get; set; }
+        private string? paramString { get; set; }
+        public Action<int>? Worker1Int { get; set; }
+        private int paramInt { get; set; }
+        public Func<Task>? WorkerAsync { get; set; }
+
+        private static ProcessingFormNET? currentInstance { get; set; }
+
+        private ProcessingFormNET(Action worker)
+        {
+            InitializeComponent();
+
+            _dropShadow.ApplyShadows(this);
+
+            if (worker == null)
+                throw new ArgumentNullException();
+
+            Worker = worker;
+        } // end of method
+
+        private ProcessingFormNET(Action<string> worker, string param)
+        {
+            InitializeComponent();
+
+            _dropShadow.ApplyShadows(this);
+
+            if (worker == null)
+                throw new ArgumentNullException();
+
+            Worker1String = worker;
+            this.paramString = param;
+            ParamType = 1;
+        } // end of method
+
+        private ProcessingFormNET(Action<int> worker, int param)
+        {
+            InitializeComponent();
+
+            _dropShadow.ApplyShadows(this);
+
+            if (worker == null)
+                throw new ArgumentNullException();
+
+            Worker1Int = worker;
+            this.paramInt = param;
+            ParamType = 2;
+        } // end of method
+
+        private ProcessingFormNET(Func<Task> worker)
+        {
+            InitializeComponent();
+
+            _dropShadow.ApplyShadows(this);
+
+            if (worker == null)
+                throw new ArgumentNullException();
+
+            WorkerAsync = worker;
+            ParamType = 3;
+        } // end of method
+
+        private void Start(string message, Form? owner)
+        {
+
+            if (this.InvokeRequired)
+            {
+                this.Invoke((MethodInvoker)delegate
+                {
+                    Start(message, owner);
+                });
+            }
+            else
+            {
+                this.PesanLabel.Text = message;
+
+                if (owner == null)
+                {
+                    this.StartPosition = FormStartPosition.CenterScreen;
+                }
+                else
+                {
+                    // Jika owner tidak null, atur posisi window muncul di tengah owner
+                    this.TopMost = owner.TopMost;
+                    this.StartPosition = FormStartPosition.CenterParent;
+                }
+                
+                this.ShowDialog(owner);
+            }
+
+
+        } // end of method
+
+        public static void Execute(string message, Action someAction)
+        {
+            // Dapatkan form yang saat ini aktif
+            Form? owner = Application.OpenForms.OfType<Form>().LastOrDefault();
+
+            // Buat instance ProcessingForm
+            ProcessingFormNET processingForm = new ProcessingFormNET(someAction);
+
+            // Set properti currentInstance
+            currentInstance = processingForm;
+
+            // Tampilkan form ProcessingForm
+            processingForm.Start(message, owner);
+        } // end of method
+
+        public static void Execute(string message, Action<string> someAction, string param)
+        {
+            // Dapatkan form yang saat ini aktif
+            Form? owner = Application.OpenForms.OfType<Form>().LastOrDefault();
+
+            // Buat instance ProcessingForm
+            ProcessingFormNET processingForm = new ProcessingFormNET(someAction, param);
+
+            // Set properti currentInstance
+            currentInstance = processingForm;
+
+            // Tampilkan form ProcessingForm
+            processingForm.Start(message, owner);
+        } // end of method
+
+        public static void Execute(string message, Action<int> someAction, int param)
+        {
+            // Dapatkan form yang saat ini aktif
+            Form? owner = Application.OpenForms.OfType<Form>().LastOrDefault();
+
+            // Buat instance ProcessingForm
+            ProcessingFormNET processingForm = new ProcessingFormNET(someAction, param);
+
+            // Set properti currentInstance
+            currentInstance = processingForm;
+
+            // Tampilkan form ProcessingForm
+            processingForm.Start(message, owner);
+        } // end of method
+
+        public static async Task ExecuteAsync(string message, Func<Task> someAction)
+        {
+
+            // Dapatkan form yang saat ini aktif
+            Form? owner = Application.OpenForms.OfType<Form>().LastOrDefault();
+
+            // Buat instance ProcessingForm
+            ProcessingFormNET processingForm = new ProcessingFormNET(someAction);
+
+            // Set properti currentInstance
+            currentInstance = processingForm;
+
+            // Tampilkan form ProcessingForm secara asinkron
+            await Task.Run(() =>
+            {
+                // Menunggu form ditampilkan dan task di dalamnya selesai
+                processingForm.Start(message, owner);
+            });
+
+        } // end of method
+
+        public static void UpdateLabel(string message)
+        {
+            if (currentInstance != null && currentInstance.InvokeRequired)
+            {
+                currentInstance.Invoke((MethodInvoker)delegate
+                {
+                    currentInstance.PesanLabel.Text = message;
+                });
+            }
+            else if (currentInstance != null)
+            {
+                currentInstance.PesanLabel.Text = message;
+            }
+        } // end of method
+
+        protected override void OnLoad(EventArgs e)
+        {
+
+            base.OnLoad(e);
+
+            switch (ParamType)
+            {
+                case 0:
+                default:
+                    Task.Factory.StartNew(() =>
+                    {
+                        Worker!();
+                    }).ContinueWith(t =>
+                    {
+                        this.Close();
+                    }, TaskScheduler.FromCurrentSynchronizationContext());
+                    break;
+                case 1:
+                    Task.Factory.StartNew(() =>
+                    {
+                        Worker1String!(paramString!);
+                    }).ContinueWith(t =>
+                    {
+                        this.Close();
+                    }, TaskScheduler.FromCurrentSynchronizationContext());
+                    break;
+                case 2:
+                    Task.Factory.StartNew(() =>
+                    {
+                        Worker1Int!(paramInt);
+                    }).ContinueWith(t =>
+                    {
+                        this.Close();
+                    }, TaskScheduler.FromCurrentSynchronizationContext());
+                    break;
+                case 3:
+                    Task.Run(async () =>
+                    {
+                        await WorkerAsync!();
+                    }).ContinueWith(t =>
+                    {
+                        this.Close();
+                    }, TaskScheduler.FromCurrentSynchronizationContext());
+                    break;
+            }
+
+        } // end of method
+    } // end of class
+} // end of namespace
---
+++ ProcessingFormNET.designer.cs
@@ -0,0 +1,96 @@
+﻿
+namespace Magic
+{
+    partial class ProcessingFormNET
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ProcessingFormNET));
+            this.PesanLabel = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.ActivationLoadingImage = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ActivationLoadingImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PesanLabel
+            // 
+            this.PesanLabel.Location = new System.Drawing.Point(0, 53);
+            this.PesanLabel.Name = "PesanLabel";
+            this.PesanLabel.Size = new System.Drawing.Size(300, 20);
+            this.PesanLabel.TabIndex = 6;
+            this.PesanLabel.Text = "Sedang menyimpan...";
+            this.PesanLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.ActivationLoadingImage);
+            this.panel1.Controls.Add(this.PesanLabel);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(300, 100);
+            this.panel1.TabIndex = 7;
+            // 
+            // ActivationLoadingImage
+            // 
+            this.ActivationLoadingImage.Image = ((System.Drawing.Image)(resources.GetObject("ActivationLoadingImage.Image")));
+            this.ActivationLoadingImage.InitialImage = null;
+            this.ActivationLoadingImage.Location = new System.Drawing.Point(140, 30);
+            this.ActivationLoadingImage.Name = "ActivationLoadingImage";
+            this.ActivationLoadingImage.Size = new System.Drawing.Size(20, 20);
+            this.ActivationLoadingImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ActivationLoadingImage.TabIndex = 10;
+            this.ActivationLoadingImage.TabStop = false;
+            // 
+            // ProcessingForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(300, 100);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ProcessingForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Silakan tunggu...";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ActivationLoadingImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox ActivationLoadingImage;
+        public System.Windows.Forms.Label PesanLabel;
+    }
+}
